207ef89ca1671d9e01541cd636aaf504b04486c8
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-02-07 02:02:49 +0530

    send messages added

---
+++ chat-house/src/Components/TheirMessage.jsx
@@ -0,0 +1,36 @@
+const TheirMessage = ({ lastMessge, message }) => {
+  const isFirstMessageByUser =
+    !lastMessge || lastMessge.sender.username !== message.sender.username;
+
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachment[0].file}
+          alt="message attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            backgroundColor: "#CABCDC",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+      TheirMessage
+    </div>
+  );
+};
+export default TheirMessage;
