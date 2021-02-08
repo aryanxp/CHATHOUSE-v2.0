53e70749e19a6291bd25233710ed3c8a44725f30
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-02-08 15:50:14 +0530

    added login page

--- chat-house/src/App.js
+++ chat-house/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
+import LoginForm from "./Components/LoginForm";
 import ChatFeed from "./Components/ChatFeed";
 
 function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
   return (
     <ChatEngine
       height="100vh"
       projectID="f3cd408b-4e3c-467b-bcab-e455038f3914"
-      userName="archit.soni"
-      userSecret="123123"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
